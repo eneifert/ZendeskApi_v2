452f0bbc3cfddcbc7089bd62ace648b71ae6d167
Ruann Botes <ruann@thespecialist.co.za>
2024-08-20 00:25:03 +0200

    feat: add models for UserField responses and data structures
    - Created models: GroupUserFieldResponse, IndividualUserFieldResponse, UserField.
    - These models handle the structure of data returned by UserField endpoints.

---
+++ src/ZendeskApi_v2/Models/Users/IndividualUserFieldResponse.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace ZendeskApi_v2.Models.Users
+{
+    public class IndividualUserFieldResponse
+    {
+        [JsonProperty("user_field")]
+        public UserField UserField { get; set; }
+    }
+}
---
+++ src/ZendeskApi_v2/Models/Users/UserField.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace ZendeskApi_v2.Models.Users
+{
+    public class UserFieldBase
+    { 
+        [JsonProperty("key")]
+        public string Key { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("raw_title")]
+        public string RawTitle { get; set; }
+
+        [JsonProperty("raw_description")]
+        public string RawDescription { get; set; }
+
+        [JsonProperty("position")]
+        public long? Position { get; set; }
+
+        [JsonProperty("active")]
+        public bool? Active { get; set; }
+
+        [JsonProperty("system")]
+        public bool? System { get; set; }
+
+        [JsonProperty("regexp_for_validation")]
+        public string RegexpForValidation { get; set; }
+
+        [JsonProperty("custom_field_options")]
+        public IList<CustomFieldOptions> CustomFieldOptions { get; set; }
+
+        [JsonProperty("tag")]
+        public string Tag { get; set; }
+    }
+
+    public class UserField : UserFieldBase
+    {
+        [JsonProperty("id")]
+        public long? Id { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("created_at")]
+        [JsonConverter(typeof(IsoDateTimeConverter))]
+        public DateTimeOffset? CreatedAt { get; set; }
+
+        [JsonProperty("updated_at")]
+        [JsonConverter(typeof(IsoDateTimeConverter))]
+        public DateTimeOffset? UpdatedAt { get; set; }
+    }
+
+    public class CustomFieldOptions : FieldOptions
+    {
+        [JsonProperty("id")]
+        public long? Id { get; set; }
+    }
+
+    public class FieldOptions
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("raw_name")]
+        public string RawName { get; set; }
+
+        [JsonProperty("value")]
+        public string Value { get; set; }
+    }
+}
